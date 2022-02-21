d5f693e7d464de8b413b761f1483897d6ef2fe79
vthanhnguyen <vthanhnguyen95@yahoo.com>
2022-02-20 19:55:46 -0800

    finished lab 1

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
+#how to run: python .\server.py
 
 from socket import *
 import sys # In order to terminate the program
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
 #Prepare a sever socket
-serverPort = 80 # assign a port number
+serverPort = 1234 # assign a port number
 serverSocket.bind(("10.0.0.5" , serverPort)) # bind the socket to ip and port
 serverSocket.listen(1)
 
@@ -19,18 +20,20 @@ while True:
         f = open(filename[1:])
         outputdata = f.read()
         #Send one HTTP header line into socket
-        connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n")
+        connectionSocket.send(b"HTTP/1.1 200 OK\r\n\r\n")
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
-
         connectionSocket.send("\r\n".encode())
         #close the connection
         connectionSocket.close() 
+
     except IOError:
         #Send response message for file not found
-        connectionSocket.send("HTTP/1.1 404 NOT FOUND\r\n\r\n")
-        
+        connectionSocket.send(b"HTTP 404 NOT FOUND\r\n\r\n")
+        connectionSocket.send(b"<html> <body> <div>HHTP REQUEST 404 NOT FOUND</div></body></html>")
         #Close client socket
         serverSocket.close()
-sys.exit()#Terminate the program after sending the corresponding data
+
+    sys.exit()#Terminate the program after sending the corresponding data
+
