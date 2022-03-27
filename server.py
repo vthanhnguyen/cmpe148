39ba78a1c83b1adaee1cd735f8ecc3c264458191
vthanhnguyen <vthanhnguyen95@yahoo.com>
2022-03-27 16:09:57 -0700

    SMTP skeleton code

--- server.py
+++ server.py
@@ -1,34 +1,20 @@
 from socket import *
-import sys # In order to terminate the program
-serverSocket = socket(AF_INET, SOCK_STREAM)
-
-#Prepare a sever socket
+msg = "\r\n I love computer networks!"
+endmsg = "\r\n.\r\n"
+# Choose a mail server (e.g. Google mail server) and call it mailserver
+mailserver = #Fill in start #Fill in end
+# Create socket called clientSocket and establish a TCP connection with mailserver
 #Fill in start
 #Fill in end
-
-while True:
-    #Establish the connection
-    print('Ready to serve...')
-    connectionSocket, addr = #Fill in start #Fill in end
-    try:
-        message = #Fill in start #Fill in end
-        filename = message.split()[1]
-        f = open(filename[1:])
-        outputdata = #Fill in start #Fill in end
-        #Send one HTTP header line into socket
-        #Fill in start
-        #Fill in end
-        #Send the content of the requested file to the client
-        for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode())
-        connectionSocket.send("\r\n".encode())
-        connectionSocket.close()
-    except IOError:
-        #Send response message for file not found
-        #Fill in start
-        #Fill in end
-        #Close client socket
-        #Fill in start
-        #Fill in end
-serverSocket.close()
-sys.exit()#Terminate the program after sending the corresponding data
+recv = clientSocket.recv(1024).decode()
+print(recv)
+if recv[:3] != '220':
+print('220 reply not received from server.')
+# Send HELO command and print server response.
+heloCommand = 'HELO Alice\r\n'
+clientSocket.send(heloCommand.encode())
+recv1 = clientSocket.recv(1024).decode()
+print(recv1)
+if recv1[:3] != '250':
+ print('250 reply not received from server.')
+ 
