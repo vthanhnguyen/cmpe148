4dc3ec3fc5e7a565631ebe0ae3669d1431fa876e
vthanhnguyen <vthanhnguyen95@yahoo.com>
2022-03-27 20:25:46 -0700

    error

--- README.md
+++ README.md
@@ -1,11 +1,2 @@
-Project Group Name: TKD
-Project Proposal: Use Arduino to transmit data and display/control it on a group-made web app.
-
-
-
-The purpose of this project is to use valuable tools we learned throughout our college career such as using our software skills to create a web-based application for our UI and using our hardware skills to set up an Arduino which includes IoT devices that will gather data. This project correlates with computer networks because we will have two computerized devices such as our computer and our Arduino that will exchange data with each other. We can use ports/sockets to communicate with the devices. All students will be required to purchase either an Arduino or an Elegoo (depending on the students' financial situation) to complete this project.
-
-
-
-
+Lab 3:SMTP LAB
 
--- server.py
+++ server.py
@@ -1,20 +1,65 @@
 from socket import *
+
 msg = "\r\n I love computer networks!"
 endmsg = "\r\n.\r\n"
+
 # Choose a mail server (e.g. Google mail server) and call it mailserver
-mailserver = #Fill in start #Fill in end
+mailServer = ('localhost',12345)
 # Create socket called clientSocket and establish a TCP connection with mailserver
-#Fill in start
-#Fill in end
+clientSocket = socket(AF_INET,SOCK_STREAM)
+clientSocket.connect(mailServer)
 recv = clientSocket.recv(1024).decode()
 print(recv)
-if recv[:3] != '220':
-print('220 reply not received from server.')
+if recv[:3] != '220': #220 is needed to ensure connectivity
+    print('220 reply not received from server.')
+
 # Send HELO command and print server response.
 heloCommand = 'HELO Alice\r\n'
 clientSocket.send(heloCommand.encode())
 recv1 = clientSocket.recv(1024).decode()
 print(recv1)
-if recv1[:3] != '250':
+if recv1[:3] != '250': #250 is used for an ack
  print('250 reply not received from server.')
  
+# Send MAIL FROM command and print server response.
+mailFrom = "MAIL FROM: <anyemailid@gmail.com> \r\n"
+clientSocket.send(mailFrom.encode())
+recv2 = clientSocket.recv(1024)
+print("After MAIL FROM command: "+recv2)
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
+
+# Send RCPT TO command and print server response.
+rcptTo = "RCPT TO: <destination@gmail.com> \r\n"
+clientSocket.send(rcptTo.encode())
+recv3 = clientSocket.recv(1024)
+print("After RCPT TO command: "+recv3)
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
+
+# Send DATA command and print server response.
+data = "DATA\r\n"
+clientSocket.send(data.encode())
+recv4 = clientSocket.recv(1024)
+print("After DATA command: "+recv4)
+
+if recv1[:3] != '250':
+    print('250 reply not received from server.')
+
+# Send message data.
+message = input('Enter message: ')
+endOfMessage = '.'
+clientSocket.send(message + endOfMessage)
+recv5 = clientSocket.recv(1024)
+
+print("Send Message Command: "+recv4)
+
+print(recv1)
+if recv1[:3] != '250': #250 is used for an ack
+ print('250 reply not received from server.')
+
+# Send QUIT command and get server response.
+clientSocket.send("QUIT\r\n".encode())
+message=clientSocket.recv(1024)
+print (message)
+clientSocket.close()
