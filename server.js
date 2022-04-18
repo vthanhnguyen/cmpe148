fb75a5a90156c77f6ce7895aa96623a88c6208de
vthanhnguyen <vthanhnguyen95@yahoo.com>
2022-04-18 03:25:57 -0700

    added led to turn on or off

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const parser = new parsers.Readline({
     delimiter: '\r\n'
 });
 //UDP request inorder to get data
-var potPort = new SerialPort('COM3',{ 
+var port = new SerialPort('COM3',{ 
     baudRate: 9600,
     dataBits: 8,
     parity: 'none',
     stopBits: 1,
     flowControl: false
 });
-potPort.pipe(parser); //pipe and parse the data
+port.pipe(parser); //pipe and parse the data
 
 //create our server to host our webpage
 var myServer = http.createServer(function(request,response){
@@ -27,19 +27,22 @@ var myServer = http.createServer(function(request,response){
 
 //use socket.io to ensure connection
 var io = require('socket.io').listen(myServer);
-io.on('connection', function(data){
+io.on('connection', function(socket){
     console.log('Client webpage is listening..');
+    socket.on('lights',function(ledData){
+        console.log(ledData);
+        port.write( ledData.status );
+
+    });
 })
 
 //using our parser, whenever we receive data we will run this function
 parser.on('data', function(data) {
     //whenever we recieve data, print it
-    console.log('Data recieved from potentialometer: ' + data);
-
-    //when we recieve data from the serialport/parser, emit a message data
+    console.log('Data traveling through the port: ' + data);
+    //when we recieve data from the serialport/parser, emit the data to the server to client
     io.emit('data', data);
 });
 
 //run the server at port localhost port 3000
 myServer.listen(3000);
-
