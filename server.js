9100054376afe0eb4c6a58b6761689906f75be29
vthanhnguyen <vthanhnguyen95@yahoo.com>
2022-04-17 19:49:22 -0700

    successfully displaying data

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+var http = require('http');
+var fs = require('fs')
+var index = fs.readFileSync('client.html')
 
 var SerialPort = require('serialport');
 const parsers = SerialPort.parsers;
@@ -16,8 +19,23 @@ var port = new SerialPort('COM3',{
 
 port.pipe(parser);
 
+var myServer = http.createServer(function(request,response){
+    //return a http 200 response code and content
+    response.writeHead(200,{'Content-Type': 'text/html'});
+    response.end(index);
+});
+
+var io = require('socket.io').listen(myServer);
+io.on('connection', function(data){
+    console.log('Client webpage is listening..');
+})
+
 parser.on('data', function(data) {
     //whenever we recieve data, print it
     console.log('Received data from port: ' + data);
-    
+
+    //when we recieve data from the serialport/parser, emit a message data
+    io.emit('data', data);
 });
+
+myServer.listen(3000);
